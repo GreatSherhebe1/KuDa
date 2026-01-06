b90760b60daa48b6e2d5b56a6ba0b58a92e8e02f
GreatSherhebe1 <AkinavaTor@yandex.ru>
2026-01-06 22:32:27 +0500

    init app

---
+++ KuDa/KuDa.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KuDa.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KuDa/KuDa.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KuDa.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
