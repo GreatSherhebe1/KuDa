93895aa3bbea1329514aacc099b42f35f84898d5
GreatSherhebe1 <AkinavaTor@yandex.ru>
2026-01-12 22:26:03 +0500

    ADD: dbContext

---
+++ KuDa/DAL/DBContext/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+namespace DAL.DBContext
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupUser> GroupUsers { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
