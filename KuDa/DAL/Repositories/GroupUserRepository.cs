61d6f136c5f608f03238b0fb0d00d6369ddb5320
GreatSherhebe1 <AkinavaTor@yandex.ru>
2026-01-12 21:58:02 +0500

    ADD: repo implementation

---
+++ KuDa/DAL/Repositories/GroupUserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Model.Entities;
+using Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class GroupUserRepository : IRepository<GroupUser>
+    {
+        public Task AddAsync(GroupUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(GroupUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GroupUser>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GroupUser> GetByIDAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(GroupUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KuDa/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Model.Entities;
+using Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        public Task AddAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIDAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
