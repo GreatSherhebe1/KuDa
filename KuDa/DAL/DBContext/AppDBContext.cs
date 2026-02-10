63d2fc501b1ea8aad58f3ee2a1831f3104ad1557
GreatSherhebe1 <AkinavaTor@yandex.ru>
2026-02-10 20:22:56 +0500

    DEL: remove dal

--- KuDa/DAL/DBContext/AppDBContext.cs
+++ KuDa/DAL/DBContext/AppDBContext.cs
@@ -5,15 +5,28 @@ namespace DAL.DBContext
 {
     public class AppDBContext : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Group> Groups { get; set; }
-        public DbSet<GroupUser> GroupUsers { get; set; }
-        public DbSet<Transaction> Transactions { get; set; }
-        public DbSet<User> Users { get; set; }
+        private readonly string connectionString;
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<GroupUser> GroupUsers { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        public AppDBContext(string connectionString) 
+        {
+            this.connectionString = connectionString;
+            Database.EnsureCreated();
+        }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base()
+        {
+
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(connectionString);
         }
     }
 }
