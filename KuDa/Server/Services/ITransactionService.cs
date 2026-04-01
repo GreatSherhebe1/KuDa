082208f0d7e316ce683c01d93cf46672f7509515
Ustinov_Nikita <unv@plm-ural.ru>
2026-04-01 19:00:37 +0500

    ADD: services

---
+++ KuDa/Server/Services/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using KuDa.Server.DTO;
+
+namespace KuDa.Server.Services
+{
+    public interface ITransactionService
+    {
+        Task<IEnumerable<TransactionResponse>> GetAllProductsAsync(CancellationToken token = default);
+        Task<TransactionResponse?> GetTransactionByIDAsync(int ID, CancellationToken token = default);
+        Task<TransactionResponse> CreateTransactionAsync(TransationRequest dto, CancellationToken token = default);
+        Task<TransactionResponse> UpdateTransactionAsync(TransationRequest dto, CancellationToken token = default);
+        Task<bool> DeleteTransactionAsync(int id, CancellationToken token = default);
+    }
+}
---
+++ KuDa/Server/Services/TransactionService.cs
@@ -0,0 +1,32 @@
+﻿using KuDa.Server.DTO;
+
+namespace KuDa.Server.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        public Task<TransactionResponse> CreateTransactionAsync(TransationRequest dto, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTransactionAsync(int id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TransactionResponse>> GetAllProductsAsync(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TransactionResponse?> GetTransactionByIDAsync(int ID, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TransactionResponse> UpdateTransactionAsync(TransationRequest dto, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
