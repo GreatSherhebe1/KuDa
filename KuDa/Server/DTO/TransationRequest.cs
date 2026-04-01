18d3da4e55e3fed2cb8aac676f99247b76032b9b
Ustinov_Nikita <unv@plm-ural.ru>
2026-04-01 18:58:51 +0500

    ADD: DTO for transactions

---
+++ KuDa/Server/DTO/TransationRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace KuDa.Server.DTO
+{
+    public record TransationRequest(
+        int ID,
+        string Description,
+        decimal Amount,
+        DateTime Date,
+
+        int CategoryID,
+        int GroupID,
+        int UserID);
+}
