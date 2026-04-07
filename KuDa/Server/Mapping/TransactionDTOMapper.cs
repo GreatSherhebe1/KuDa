b7b2c8299045eae68685f0da0bd6941380df103e
Ustinov_Nikita <unv@plm-ural.ru>
2026-04-07 18:16:05 +0500

    ADD: mappers

---
+++ KuDa/Server/Mapping/TransactionDTOMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using KuDa.Server.DTO;
+using Model.Entities;
+
+namespace KuDa.Server.Mapping
+{
+    public class TransactionDTOMapper : Profile
+    {
+        public TransactionDTOMapper() 
+        {
+            CreateMap<Transaction, TransactionResponse>();
+            CreateMap<TransactionResponse, Transaction>();
+            CreateMap<TransationRequest, Transaction>();
+        }
+    }
+}
---
+++ KuDa/Server/Mapping/UserDTOMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using KuDa.Server.DTO;
+using Model.Entities;
+
+namespace KuDa.Server.Mapping
+{
+    public class UserDTOMapper : Profile
+    {
+        public UserDTOMapper() 
+        {
+            CreateMap<User, UserResponse>();
+            CreateMap<UserResponse, User>();
+            CreateMap<UserRequest, User>();
+        }
+    }
+}
